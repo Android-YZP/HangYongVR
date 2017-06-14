5677c667561c9caf90eb44a697ce8aa6a0145ab8
wzq930102 <724784530@qq.com>
2017-06-14 15:54:13 +0800

    nihg12

--- app/src/main/java/com/jt/base/login/ForgetActivity.java
+++ app/src/main/java/com/jt/base/login/ForgetActivity.java
@@ -1,7 +1,11 @@
 package com.jt.base.login;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
 
 import com.google.gson.Gson;
 import com.jt.base.R;
@@ -17,6 +21,10 @@ public class ForgetActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            //透明状态栏
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forget);
 
