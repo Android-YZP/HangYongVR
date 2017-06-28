64407719d41af1d336f415b9d20617fd35d437a1
Smith2 <724784530@qq.com>
2017-06-28 09:40:58 +0800

    Merge branch 'VR' of http://192.168.1.129:10101/r/AndroidVR into VR
    # Conflicts:
    #	app/src/main/res/layout/activity_personal.xml
    #	app/src/main/res/layout/fragment_my.xml

--- app/src/main/java/com/jt/base/http/responsebean/GetRoomBean.java
+++ app/src/main/java/com/jt/base/http/responsebean/GetRoomBean.java
@@ -8,11 +8,12 @@ import java.util.List;
 
 public class GetRoomBean {
 
+
     /**
      * code : 0
      * msg : success
-     * result : [{"id":null,"uid":null,"channelName":null,"isall":0,"status":null,"price":0,"img":"2014.gif","head":"2014.gif","createDate":null,"alipay":null,"place":null,"outputsourcetype":null,"upstreamAddress":null,"rtmpDownstreamAddress":null,"flvDownstreamAddress":null,"hlsDownstreamAddress":null,"channelId":null,"playerpassword":null,"watermarkid":null,"outputrateOne":null,"outputrateTow":null,"outputrateThree":null}]
-     * page : {"total":0,"totalPage":0,"page":1,"count":10,"startDate":null,"endDate":null,"keywords":null,"order":null,"asc":null,"partition":null,"status":null,"getAllRecord":false,"countAllRecord":true,"channelName":null,"startIndex":0}
+     * result : [{"id":null,"uid":null,"username":null,"channelName":"测试2D","isall":0,"status":null,"price":0,"img":"43631035-02cd-428d-8861-70688142d866.jpg","head":"0f4d349d-f0ed-4a43-910e-73f0d627dc6f.jpg","createDate":null,"alipay":null,"place":null,"outputsourcetype":null,"upstreamAddress":null,"rtmpDownstreamAddress":"rtmp://9250.liveplay.myqcloud.com/live/9250_34cfb6aeee6d4ee3b52ab77a0ac255a8","flvDownstreamAddress":"http://9250.liveplay.myqcloud.com/live/9250_34cfb6aeee6d4ee3b52ab77a0ac255a8.flv","hlsDownstreamAddress":null,"channelId":"9250_34cfb6aeee6d4ee3b52ab77a0ac255a8","playerpassword":null,"watermarkid":null,"outputrateOne":null,"outputrateTow":null,"outputrateThree":null,"maxnum":null,"clicknum":null,"typeId":1,"typeName":null,"topicId":7,"topicName":null,"img1":"754c8d2e-bb72-4d14-b686-08ee73215b91.jpg","size":null,"type":null,"sort":null},{"id":null,"uid":null,"username":null,"channelName":"测试全景","isall":2,"status":null,"price":10,"img":"70fcb353-91d1-47a0-a2b9-97634f30b4f0.jpg","head":"e224c53c-b2bc-49c5-a68b-66901ff5653a.jpg","createDate":null,"alipay":null,"place":null,"outputsourcetype":null,"upstreamAddress":null,"rtmpDownstreamAddress":"rtmp://9250.liveplay.myqcloud.com/live/9250_19e3ff51157143fd9f559b8980ace1c9","flvDownstreamAddress":"http://9250.liveplay.myqcloud.com/live/9250_19e3ff51157143fd9f559b8980ace1c9.flv","hlsDownstreamAddress":"http://9250.liveplay.myqcloud.com/9250_19e3ff51157143fd9f559b8980ace1c9.m3u8","channelId":"9250_19e3ff51157143fd9f559b8980ace1c9","playerpassword":null,"watermarkid":null,"outputrateOne":null,"outputrateTow":null,"outputrateThree":null,"maxnum":null,"clicknum":null,"typeId":1,"typeName":null,"topicId":9,"topicName":null,"img1":"c16366c6-d0dd-452e-b5a1-f5203e1409c7.jpg","size":null,"type":null,"sort":null}]
+     * page : {"total":2,"totalPage":1,"page":1,"count":3,"startDate":null,"endDate":null,"keywords":null,"order":null,"asc":null,"partition":null,"status":null,"getAllRecord":false,"countAllRecord":true,"channelName":null,"startIndex":0}
      */
 
     private int code;
@@ -54,10 +55,10 @@ public class GetRoomBean {
 
     public static class PageBean {
         /**
-         * total : 0
-         * totalPage : 0
+         * total : 2
+         * totalPage : 1
          * page : 1
-         * count : 10
+         * count : 3
          * startDate : null
          * endDate : null
          * keywords : null
@@ -212,31 +213,43 @@ public class GetRoomBean {
         /**
          * id : null
          * uid : null
-         * channelName : null
+         * username : null
+         * channelName : 测试2D
          * isall : 0
          * status : null
          * price : 0
-         * img : 2014.gif
-         * head : 2014.gif
+         * img : 43631035-02cd-428d-8861-70688142d866.jpg
+         * head : 0f4d349d-f0ed-4a43-910e-73f0d627dc6f.jpg
          * createDate : null
          * alipay : null
          * place : null
          * outputsourcetype : null
          * upstreamAddress : null
-         * rtmpDownstreamAddress : null
-         * flvDownstreamAddress : null
+         * rtmpDownstreamAddress : rtmp://9250.liveplay.myqcloud.com/live/9250_34cfb6aeee6d4ee3b52ab77a0ac255a8
+         * flvDownstreamAddress : http://9250.liveplay.myqcloud.com/live/9250_34cfb6aeee6d4ee3b52ab77a0ac255a8.flv
          * hlsDownstreamAddress : null
-         * channelId : null
+         * channelId : 9250_34cfb6aeee6d4ee3b52ab77a0ac255a8
          * playerpassword : null
          * watermarkid : null
          * outputrateOne : null
          * outputrateTow : null
          * outputrateThree : null
+         * maxnum : null
+         * clicknum : null
+         * typeId : 1
+         * typeName : null
+         * topicId : 7
+         * topicName : null
+         * img1 : 754c8d2e-bb72-4d14-b686-08ee73215b91.jpg
+         * size : null
+         * type : null
+         * sort : null
          */
 
         private Object id;
         private Object uid;
-        private Object channelName;
+        private Object username;
+        private String channelName;
         private int isall;
         private Object status;
         private int price;
@@ -247,15 +260,25 @@ public class GetRoomBean {
         private Object place;
         private Object outputsourcetype;
         private Object upstreamAddress;
-        private Object rtmpDownstreamAddress;
-        private Object flvDownstreamAddress;
+        private String rtmpDownstreamAddress;
+        private String flvDownstreamAddress;
         private Object hlsDownstreamAddress;
-        private Object channelId;
+        private String channelId;
         private Object playerpassword;
         private Object watermarkid;
         private Object outputrateOne;
         private Object outputrateTow;
         private Object outputrateThree;
+        private Object maxnum;
+        private Object clicknum;
+        private int typeId;
+        private Object typeName;
+        private int topicId;
+        private Object topicName;
+        private String img1;
+        private Object size;
+        private Object type;
+        private Object sort;
 
         public Object getId() {
             return id;
@@ -273,11 +296,19 @@ public class GetRoomBean {
             this.uid = uid;
         }
 
-        public Object getChannelName() {
+        public Object getUsername() {
+            return username;
+        }
+
+        public void setUsername(Object username) {
+            this.username = username;
+        }
+
+        public String getChannelName() {
             return channelName;
         }
 
-        public void setChannelName(Object channelName) {
+        public void setChannelName(String channelName) {
             this.channelName = channelName;
         }
 
@@ -361,19 +392,19 @@ public class GetRoomBean {
             this.upstreamAddress = upstreamAddress;
         }
 
-        public Object getRtmpDownstreamAddress() {
+        public String getRtmpDownstreamAddress() {
             return rtmpDownstreamAddress;
         }
 
-        public void setRtmpDownstreamAddress(Object rtmpDownstreamAddress) {
+        public void setRtmpDownstreamAddress(String rtmpDownstreamAddress) {
             this.rtmpDownstreamAddress = rtmpDownstreamAddress;
         }
 
-        public Object getFlvDownstreamAddress() {
+        public String getFlvDownstreamAddress() {
             return flvDownstreamAddress;
         }
 
-        public void setFlvDownstreamAddress(Object flvDownstreamAddress) {
+        public void setFlvDownstreamAddress(String flvDownstreamAddress) {
             this.flvDownstreamAddress = flvDownstreamAddress;
         }
 
@@ -385,11 +416,11 @@ public class GetRoomBean {
             this.hlsDownstreamAddress = hlsDownstreamAddress;
         }
 
-        public Object getChannelId() {
+        public String getChannelId() {
             return channelId;
         }
 
-        public void setChannelId(Object channelId) {
+        public void setChannelId(String channelId) {
             this.channelId = channelId;
         }
 
@@ -431,6 +462,86 @@ public class GetRoomBean {
 
         public void setOutputrateThree(Object outputrateThree) {
             this.outputrateThree = outputrateThree;
+        }
+
+        public Object getMaxnum() {
+            return maxnum;
+        }
+
+        public void setMaxnum(Object maxnum) {
+            this.maxnum = maxnum;
+        }
+
+        public Object getClicknum() {
+            return clicknum;
+        }
+
+        public void setClicknum(Object clicknum) {
+            this.clicknum = clicknum;
+        }
+
+        public int getTypeId() {
+            return typeId;
+        }
+
+        public void setTypeId(int typeId) {
+            this.typeId = typeId;
+        }
+
+        public Object getTypeName() {
+            return typeName;
+        }
+
+        public void setTypeName(Object typeName) {
+            this.typeName = typeName;
+        }
+
+        public int getTopicId() {
+            return topicId;
+        }
+
+        public void setTopicId(int topicId) {
+            this.topicId = topicId;
+        }
+
+        public Object getTopicName() {
+            return topicName;
+        }
+
+        public void setTopicName(Object topicName) {
+            this.topicName = topicName;
+        }
+
+        public String getImg1() {
+            return img1;
+        }
+
+        public void setImg1(String img1) {
+            this.img1 = img1;
+        }
+
+        public Object getSize() {
+            return size;
+        }
+
+        public void setSize(Object size) {
+            this.size = size;
+        }
+
+        public Object getType() {
+            return type;
+        }
+
+        public void setType(Object type) {
+            this.type = type;
+        }
+
+        public Object getSort() {
+            return sort;
+        }
+
+        public void setSort(Object sort) {
+            this.sort = sort;
         }
     }
 }
