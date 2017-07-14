0ca975f11e4e96fca9995b45a34a7fc6f888c94a
wzq930102 <724784530@qq.com>
2017-07-14 10:05:52 +0800

    Merge branch 'VR' of http://192.168.1.129:10101/r/AndroidVR into VR
    # Conflicts:
    #	app/src/main/java/com/jt/base/videos/adapters/MainAdapter.java
    #	app/src/main/java/com/jt/base/videos/fragments/MyFragment.java

--- app/src/main/java/com/jt/base/videos/fragments/SearchFragment.java
+++ app/src/main/java/com/jt/base/videos/fragments/SearchFragment.java
@@ -1,11 +1,13 @@
 package com.jt.base.videos.fragments;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 
 import com.jt.base.R;
 
@@ -24,4 +26,12 @@ public class SearchFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_search, container, false);
     }
 
-}
+
+    }
+
+
+
+
+
+
+
