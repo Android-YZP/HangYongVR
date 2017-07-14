af62241db513974adec6d3ab388d88d82baa2a82
Smith2 <724784530@qq.com>
2017-07-14 15:42:58 +0800

    Merge branch 'VR' of http://192.168.1.129:10101/r/AndroidVR into VR
    # Conflicts:
    #	app/src/main/res/layout/vedio_list_activity.xml

--- app/src/main/java/com/jt/base/videos/fragments/SearchFragment.java
+++ app/src/main/java/com/jt/base/videos/fragments/SearchFragment.java
@@ -1,18 +1,32 @@
 package com.jt.base.videos.fragments;
 
 import android.annotation.SuppressLint;
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.inputmethod.InputMethodManager;
+import android.widget.ImageView;
 
 import com.jt.base.R;
+import com.jt.base.videos.adapters.MainAdapter;
+import com.jt.base.videos.adapters.MainVideosAdapter;
+import com.jt.base.videos.adapters.SearchAdapter;
+import com.jt.base.videos.adapters.SearchHeadTypeAdapter;
+
+import static com.jt.base.R.drawable.position;
 
 @SuppressLint("ValidFragment")
 public class SearchFragment extends Fragment {
+
+    private RecyclerView mRvsearchvideo;
+    private RecyclerView mRvsearchtopic;
+    private ImageView mImsearch;
+    private RecyclerView mRvsearchTopic;
+    private SearchAdapter mSearchAdapter;
+    private SearchHeadTypeAdapter mSearchHeadTypeAdapter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -23,11 +37,43 @@ public class SearchFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View v = inflater.inflate(R.layout.fragment_search, container, false);
+        initView(v);
+        initRecyclerView();
+        return v;
+    }
+
+    private void initView(View v) {
+        mRvsearchvideo = (RecyclerView) v.findViewById(R.id.rv_search_video);
     }
 
 
+    private void initRecyclerView() {
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
+        mRvsearchvideo.setNestedScrollingEnabled(false);
+        mRvsearchvideo.setLayoutManager(linearLayoutManager);
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        mSearchAdapter = new SearchAdapter(getActivity());
+        setHead();
+        mRvsearchvideo.setAdapter(mSearchAdapter);
     }
+
+
+    //设置话题的头部局
+    private void setHead() {
+        View view = View.inflate(getContext(), R.layout.search_head, null);
+        RecyclerView SearchHead = (RecyclerView) view.findViewById(R.id.rv_search_topic_head);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
+        SearchHead.setNestedScrollingEnabled(false);
+        SearchHead.setLayoutManager(linearLayoutManager);
+        linearLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        mSearchHeadTypeAdapter = new SearchHeadTypeAdapter(getActivity());
+        SearchHead.setAdapter(mSearchHeadTypeAdapter);
+        mSearchAdapter.setHeaderView(view);
+    }
+
+
+}
 
 
 
