963c190c88f4652fa9f2d02eb84927ce00433227
Smith2 <724784530@qq.com>
2017-07-27 16:10:47 +0800

    优化播放器代码

--- app/src/main/java/com/jt/base/videoDetails/adapters/RvAdapter.java
+++ app/src/main/java/com/jt/base/videoDetails/adapters/RvAdapter.java
@@ -196,7 +196,7 @@ public class RvAdapter extends RecyclerView.Adapter<RvAdapter.MyViewHolder> {
 
         final View dialogView = LayoutInflater.from(context)
                 .inflate(R.layout.dialog_pay_item, null);
-        ImageView ivPayChacha = (ImageView) dialogView.findViewById(R.id.iv_pay_chacha);
+        Button ivPayChacha = (Button) dialogView.findViewById(R.id.btn_pay_cancel);
         TextView TvPayDiaprice = (TextView) dialogView.findViewById(R.id.tv_play_dia_price);
         TvPayDiaprice.setText("价格： " + mRoomLists.get(position).getPrice() + "元");
         TextView TvPayId = (TextView) dialogView.findViewById(R.id.tv_play_dia_id);
