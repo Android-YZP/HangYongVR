1a6febf520a8d6df9796b2db9b2e1def3dacd456
Smith2 <724784530@qq.com>
2017-08-09 11:40:41 +0800

    继承推流包

---
+++ app/src/main/java/com/hy/vrfrog/main/living/push/PushActivity.java
@@ -0,0 +1,14 @@
+package com.hy.vrfrog.main.living.push;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.hy.vrfrog.R;
+
+public class PushActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_push);
+    }
+}
