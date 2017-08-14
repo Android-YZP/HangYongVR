8df7e2f2efefafc056cbd5ebd683589ccd578c7f
Smith2 <724784530@qq.com>
2017-08-14 09:45:00 +0800

    Merge branch 'VR' of http://192.168.1.129:10101/r/AndroidVR into VR
    # Conflicts:
    #	app/src/main/java/com/hy/vrfrog/main/activitys/Main2Activity.java

--- app/src/main/java/com/hy/vrfrog/main/living/push/tesyt.java
+++
@@ -1,8 +0,0 @@
-package com.hy.vrfrog.main.living.push;
-
-/**
- * Created by 姚中平 on 2017/8/7.
- */
-
-public class tesyt {
-}
--- app/src/main/java/living/fdsfs.java
+++
@@ -1,8 +0,0 @@
-package living;
-
-/**
- * Created by 姚中平 on 2017/8/7.
- */
-
-public class fdsfs {
-}
